2ab0c8dedbc5189da41a296296520562f0d84392
marcostorresperez <torresperezmarcos93@gmail.com>
2020-12-08 16:00:18 +0100

    Terminada

---
+++ app/src/main/java/com/example/ejemplo_fragments_dinamicos/MainActivity.java
@@ -0,0 +1,70 @@
+package com.example.ejemplo_fragments_dinamicos;
+
+import android.os.Bundle;
+
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
+import com.google.android.material.snackbar.Snackbar;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
+import androidx.fragment.app.Fragment;
+
+import android.view.View;
+
+import android.view.Menu;
+import android.view.MenuItem;
+
+public class MainActivity extends AppCompatActivity {
+    boolean cargarFragment2 = true;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        Toolbar toolbar = findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+        FloatingActionButton fab = findViewById(R.id.fab);
+        fab.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Fragment f = null;
+
+                if (cargarFragment2) {
+                    f = new SecondFragment();
+                } else {
+                    f = new FirstFragment();
+                }
+
+                getSupportFragmentManager().beginTransaction().replace(R.id.contenedor, f).commit();
+
+                cargarFragment2 = false;
+            }
+        });
+
+        // Rescatamos el contenedor y le damos un fragment
+        getSupportFragmentManager().beginTransaction().add(R.id.contenedor, new FirstFragment()).commit();
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+}
